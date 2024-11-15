1a37b70c370aa4bbbf6a62efbcb5cfbf34501657
Bibibush <rlawlsgus125@gmail.com>
2024-11-16 00:57:45 +0900

    menu를 객체리스트로 만든다음 매핑하여 ui 구현

--- src/mainPage/AuthenticatedLayout/SideBar/index.tsx
+++ src/mainPage/AuthenticatedLayout/SideBar/index.tsx
@@ -1,16 +1,38 @@
 import DashboardIcon from "@/components/ui/menuIcons/DashboardIcon";
+import WalletIcon from "@/components/ui/menuIcons/WalletIcon";
+import { IoMail } from "react-icons/io5";
 
 function SideBar() {
+  const menuList = [
+    {
+      menuName: "Dashboard",
+      icon: <DashboardIcon color="#84828A" />,
+    },
+    {
+      menuName: "Exchange",
+      icon: <WalletIcon color="#84828A" />,
+    },
+    {
+      menuName: "Contact",
+      icon: <IoMail color="#84828A" size={24} />,
+    },
+  ];
+
   return (
     <aside className="bg-white lg:w-[315px] h-full flex flex-col p-5">
       <div className="text-2xl font-medium text-[#2C2C2C] mt-5">Foxstocks</div>
 
-      <div className="flex flex-col gap-4 mt-14">
+      <div className="flex flex-col gap-3 mt-14">
         <p className="text-sm font-medium text-[#7E7D82]">User Panel</p>
-        <div className="px-4 py-5 lg:w-[275px] text-[#84828A] flex gap-3 hover:bg-[#EFE9FF] rounded-md duration-150 cursor-pointer">
-          <DashboardIcon color="#84828A" />
-          Dashboard
-        </div>
+        {menuList.map((menu) => (
+          <div
+            className="px-4 py-5 lg:w-[275px] text-[#84828A] flex gap-3 hover:bg-[#EFE9FF] rounded-md duration-200 cursor-pointer"
+            key={menu.menuName}
+          >
+            {menu.icon}
+            {menu.menuName}
+          </div>
+        ))}
       </div>
     </aside>
   );
