3c98189f3743716c814364e877f5352f6bd6ac28
bibibush_jinhyeong <rlawlsgus125@gmail.com>
2024-11-27 13:10:49 +0900

    사이즈 적용

--- src/components/Dashboard/Balance/index.tsx
+++ src/components/Dashboard/Balance/index.tsx
@@ -2,11 +2,11 @@ import { Input } from "@/components/ui/input";
 
 function Balance() {
   return (
-    <section className="bg-white lg:py-5 lg:px-3 rounded-lg">
+    <section className="bg-white lg:py-5 lg:px-3 rounded-lg lg:w-[340px]">
       <p>Balance</p>
-      <div className="flex gap-2">
+      <div className="flex gap-2 mb-5">
         <Input
-          className="lg:py-4 lg:px-5 lg:w-[252px] lg:h-14 bg-[#6425FE] text-white font-medium lg:text-xl"
+          className="lg:py-4 lg:px-5 lg:w-[calc(100%-8px-63px)] lg:h-14 bg-[#6425FE] text-white font-medium lg:text-xl"
           value="$14,032.56"
           readOnly
         />
@@ -14,6 +14,12 @@ function Balance() {
           5.63%
         </div>
       </div>
+      <p>Invested</p>
+      <Input
+        className="text-white bg-black lg:h-14 lg:py-4 lg:px-5 lg:text-xl font-medium"
+        value="$7,532.21"
+        readOnly
+      />
     </section>
   );
 }
